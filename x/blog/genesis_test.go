bd98c2cefe9be065b079a43ceb8c89452e32f897
root <root@DESKTOP-PHM45I0>
2023-07-05 20:30:29 +0800

    add files

---
+++ x/blog/genesis_test.go
@@ -0,0 +1,42 @@
+package blog_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "planet/testutil/keeper"
+	"planet/testutil/nullify"
+	"planet/x/blog"
+	"planet/x/blog/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+		PortId: types.PortID,
+		PostList: []types.Post{
+			{
+				Id: 0,
+			},
+			{
+				Id: 1,
+			},
+		},
+		PostCount: 2,
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.BlogKeeper(t)
+	blog.InitGenesis(ctx, *k, genesisState)
+	got := blog.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	require.Equal(t, genesisState.PortId, got.PortId)
+
+	require.ElementsMatch(t, genesisState.PostList, got.PostList)
+	require.Equal(t, genesisState.PostCount, got.PostCount)
+	// this line is used by starport scaffolding # genesis/test/assert
+}
---
+++ x/blog/types/genesis.go
@@ -0,0 +1,42 @@
+package types
+
+import (
+	"fmt"
+	host "github.com/cosmos/ibc-go/v7/modules/core/24-host"
+)
+
+// DefaultIndex is the default global index
+const DefaultIndex uint64 = 1
+
+// DefaultGenesis returns the default genesis state
+func DefaultGenesis() *GenesisState {
+	return &GenesisState{
+		PortId:   PortID,
+		PostList: []Post{},
+		// this line is used by starport scaffolding # genesis/types/default
+		Params: DefaultParams(),
+	}
+}
+
+// Validate performs basic genesis state validation returning an error upon any
+// failure.
+func (gs GenesisState) Validate() error {
+	if err := host.PortIdentifierValidator(gs.PortId); err != nil {
+		return err
+	}
+	// Check for duplicated ID in post
+	postIdMap := make(map[uint64]bool)
+	postCount := gs.GetPostCount()
+	for _, elem := range gs.PostList {
+		if _, ok := postIdMap[elem.Id]; ok {
+			return fmt.Errorf("duplicated id for post")
+		}
+		if elem.Id >= postCount {
+			return fmt.Errorf("post id should be lower or equal than the last id")
+		}
+		postIdMap[elem.Id] = true
+	}
+	// this line is used by starport scaffolding # genesis/types/validate
+
+	return gs.Params.Validate()
+}
