6152a6e71d1606d7f6ea82fa57c9cb6bb2a8af99
root <root@DESKTOP-PHM45I0>
2023-07-05 21:08:17 +0800

    add config

--- x/blog/keeper/ibc_post.go
+++ x/blog/keeper/ibc_post.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"errors"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -41,6 +42,18 @@ func (k Keeper) OnRecvIbcPostPacket(ctx sdk.Context, packet channeltypes.Packet,
 	}
 
 	// TODO: packet reception logic
+	id := k.AppendPost(
+        ctx,
+        types.Post{
+            Creator: packet.SourcePort + "-" + packet.SourceChannel + "-" + data.Creator,
+            Title:   data.Title,
+            Content: data.Content,
+        },
+    )
+
+    packetAck.PostID = strconv.FormatUint(id, 10)
+
+
 
 	return packetAck, nil
 }
@@ -66,6 +79,16 @@ func (k Keeper) OnAcknowledgementIbcPostPacket(ctx sdk.Context, packet channelty
 
 		// TODO: successful acknowledgement logic
 
+		k.AppendSentPost(
+			ctx,
+			types.SentPost {
+				Creator: data.Creator,
+				PostID: packetAck.PostID,
+				Title: data.Title,
+				Chain: packet.DestinationPort + "-" + packet.DestinationChannel,
+			},
+		)
+
 		return nil
 	default:
 		// The counter-party module doesn't implement the correct acknowledgment format
@@ -75,8 +98,14 @@ func (k Keeper) OnAcknowledgementIbcPostPacket(ctx sdk.Context, packet channelty
 
 // OnTimeoutIbcPostPacket responds to the case where a packet has not been transmitted because of a timeout
 func (k Keeper) OnTimeoutIbcPostPacket(ctx sdk.Context, packet channeltypes.Packet, data types.IbcPostPacketData) error {
-
 	// TODO: packet timeout logic
-
+	k.AppendTimedoutPost(
+        ctx,
+        types.TimedoutPost{
+            Creator: data.Creator,
+            Title:   data.Title,
+            Chain:   packet.DestinationPort + "-" + packet.DestinationChannel,
+        },
+    )
 	return nil
 }
